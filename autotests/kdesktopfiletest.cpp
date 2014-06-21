KDE/kconfig

commit 988f09bb051dca0437ecec431ee44ed5b4a560d8

Fix locale-aware reading in KDesktopFile
The underlying KConfig used QLocale::name() for getting the locale
aware part. But this returns "de_DE" while the desktop files store
"de".

In addition it constructs a QLocale object instead of using the
system locale. This has the advantage that the usage of
QLocale::setDafault() gets honored by KConfig.

REVIEW: 118564

autotests/kdesktopfiletest.cpp
@@ -16,6 +16,7 @@
  *  Boston, MA 02110-1301, USA.
  */
 #include "kdesktopfiletest.h"
+#include "helper.h"
 #include <kconfiggroup.h>
 #include <qtemporaryfile.h>
 
@@ -47,6 +48,35 @@ void KDesktopFileTest::testRead()
     QCOMPARE(df.readIcon(), QString::fromLatin1("foo"));
     QVERIFY(!df.hasLinkType());
     QCOMPARE(df.fileName(), QFileInfo(fileName).canonicalFilePath());
+}
+
+void KDesktopFileTest::testReadLocalized()
+{
+    QTemporaryFile file("testReadLocalizedXXXXXX.desktop");
+    QVERIFY(file.open());
+    const QString fileName = file.fileName();
+    QTextStream ts(&file);
+    ts <<
+       "[Desktop Entry]\n"
+       "Type=Application\n"
+       "Name=My Application\n"
+       "Name[de]=Meine Anwendung\n"
+       "Icon=foo\n"
+       "\n";
+    file.close();
+    QVERIFY(QFile::exists(fileName));
+    QVERIFY(KDesktopFile::isDesktopFile(fileName));
+
+    DefaultLocale defaultLocale;
+    // set language to German for which we have a translation
+    QLocale::setDefault(QLocale(QLocale::German));
+    KDesktopFile df(fileName);
+    QCOMPARE(df.readName(), QString::fromLatin1("Meine Anwendung"));
+
+    // set language to French for which we don't have a translation
+    QLocale::setDefault(QLocale(QLocale::French));
+    KDesktopFile df2(fileName);
+    QCOMPARE(df2.readName(), QString::fromLatin1("My Application"));
 }
 
 void KDesktopFileTest::testSuccessfulTryExec()

autotests/kdesktopfiletest.h
@@ -26,6 +26,7 @@ class KDesktopFileTest : public QObject
     Q_OBJECT
 private Q_SLOTS:
     void testRead();
+    void testReadLocalized();
     void testUnsuccessfulTryExec();
     void testSuccessfulTryExec();
     void testActionGroup();

src/core/kconfig.cpp
@@ -95,7 +95,7 @@ KConfigPrivate::KConfigPrivate(KConfig::OpenFlags flags,
 //        mappingsRegistered = true;
 //    }
 
-    setLocale(QLocale::system().name());
+    setLocale(QLocale().name().split(QStringLiteral("_")).at(0));
 }
 
 bool KConfigPrivate::lockLocal()

autotests/helper.h
@@ -1,0 +1,43 @@
+/*
+ * Copyright 2014  Martin Gräßlin <mgraesslin@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef KCONFIG_AUTOTESTS_HELPER_H
+#define KCONFIG_AUTOTESTS_HELPER_H
+
+#include <QLocale>
+
+/**
+ * @brief Helper to store the current default QLocale and resetting to it in the dtor.
+ **/
+class DefaultLocale
+{
+public:
+    DefaultLocale()
+    : m_defaultLocale()
+    {
+    }
+    ~DefaultLocale()
+    {
+        QLocale::setDefault(m_defaultLocale);
+    }
+private:
+    QLocale m_defaultLocale;
+};
+
+#endif

autotests/kconfigtest.cpp
@@ -21,6 +21,7 @@
 #undef QT_NO_CAST_FROM_BYTEARRAY
 
 #include "kconfigtest.h"
+#include "helper.h"
 
 #include <QtTest/QtTest>
 #include <qtemporarydir.h>
@@ -927,6 +928,8 @@ void KConfigTest::testCascadingWithLocale()
 
 void KConfigTest::testMerge()
 {
+    DefaultLocale defaultLocale;
+    QLocale::setDefault(QLocale::c());
     KConfig config(TEST_SUBDIR "mergetest", KConfig::SimpleConfig);
 
     KConfigGroup cg = config.group("some group");

commit a52b00f31eb5df1cd603975bdd31794233898a59

Fix reading of entries for language/country combinations
This fixes a regression introduced in
988f09bb051dca0437ecec431ee44ed5b4a560d8.

The mentioned commit ensures that if the locale is e.g. "de_DE" the
entry "de" will be used. But this breaks if there is a translation
for another country. E.g. for "de_CH" it would also pick the "de"
entry.

This change now operates on both just the language code and the locale.
If an entry with the language code is present it will be picked. If
another entry with the exact locale is found it will be overwritten.
Modifiers are not supported as this is currently missing in QLocale.

REVIEW: 118692

autotests/kdesktopfiletest.cpp
@@ -50,6 +50,29 @@ void KDesktopFileTest::testRead()
     QCOMPARE(df.fileName(), QFileInfo(fileName).canonicalFilePath());
 }
 
+void KDesktopFileTest::testReadLocalized_data()
+{
+    QTest::addColumn<QLocale>("locale");
+    QTest::addColumn<QString>("translation");
+
+    const QString german = QStringLiteral("Meine Anwendung");
+    const QString swiss  = QStringLiteral("Mein Anwendungsli");
+
+    QTest::newRow("de")           << QLocale(QLocale::German)                << german;
+    QTest::newRow("de_DE")        << QLocale(QStringLiteral("de_DE"))        << german;
+    QTest::newRow("de_DE@bayern") << QLocale(QStringLiteral("de_DE@bayern")) << german;
+    QTest::newRow("de@bayern")    << QLocale(QStringLiteral("de@bayern"))    << german;
+    QTest::newRow("de@freiburg")  << QLocale(QStringLiteral("de@freiburg"))  << QStringLiteral("Mein Anwendungsle");
+    // For CH we have a special translation
+    QTest::newRow("de_CH")      << QLocale(QLocale::German, QLocale::Switzerland) << swiss;
+    QTest::newRow("de_CH@bern") << QLocale(QStringLiteral("de_CH@bern"))          << swiss;
+    // Austria should fall back to "de"
+    QTest::newRow("de_AT")       << QLocale(QLocale::German, QLocale::QLocale::Austria) << german;
+    QTest::newRow("de_AT@tirol") << QLocale(QStringLiteral("de_AT@tirol"))              << german;
+    // no translation for French
+    QTest::newRow("fr") << QLocale(QLocale::French) << QStringLiteral("My Application");
+}
+
 void KDesktopFileTest::testReadLocalized()
 {
     QTemporaryFile file("testReadLocalizedXXXXXX.desktop");
@@ -61,6 +84,8 @@ void KDesktopFileTest::testReadLocalized()
        "Type=Application\n"
        "Name=My Application\n"
        "Name[de]=Meine Anwendung\n"
+       "Name[de@freiburg]=Mein Anwendungsle\n"
+       "Name[de_CH]=Mein Anwendungsli\n"
        "Icon=foo\n"
        "\n";
     file.close();
@@ -68,15 +93,13 @@ void KDesktopFileTest::testReadLocalized()
     QVERIFY(KDesktopFile::isDesktopFile(fileName));
 
     DefaultLocale defaultLocale;
-    // set language to German for which we have a translation
-    QLocale::setDefault(QLocale(QLocale::German));
-    KDesktopFile df(fileName);
-    QCOMPARE(df.readName(), QString::fromLatin1("Meine Anwendung"));
 
-    // set language to French for which we don't have a translation
-    QLocale::setDefault(QLocale(QLocale::French));
-    KDesktopFile df2(fileName);
-    QCOMPARE(df2.readName(), QString::fromLatin1("My Application"));
+    QFETCH(QLocale, locale);
+    QLocale::setDefault(locale);
+    KDesktopFile df(fileName);
+
+    QEXPECT_FAIL("de@freiburg", "QLocale doesn't support modifiers", Continue);
+    QTEST(df.readName(), "translation");
 }
 
 void KDesktopFileTest::testSuccessfulTryExec()

autotests/kdesktopfiletest.h
@@ -26,6 +26,7 @@ class KDesktopFileTest : public QObject
     Q_OBJECT
 private Q_SLOTS:
     void testRead();
+    void testReadLocalized_data();
     void testReadLocalized();
     void testUnsuccessfulTryExec();
     void testSuccessfulTryExec();

src/core/kconfig.cpp
@@ -95,7 +95,7 @@ KConfigPrivate::KConfigPrivate(KConfig::OpenFlags flags,
 //        mappingsRegistered = true;
 //    }
 
-    setLocale(QLocale().name().split(QStringLiteral("_")).at(0));
+    setLocale(QLocale().name());
 }
 
 bool KConfigPrivate::lockLocal()

src/core/kconfigdata.cpp
@@ -145,6 +145,11 @@ bool KEntryMap::setEntry(const QByteArray &group, const QByteArray &key, const Q
     }
     e.bExpand = (options & EntryExpansion);
     e.bReverted = false;
+    if (options & EntryLocalized) {
+        e.bLocalizedCountry = (options & EntryLocalizedCountry);
+    } else {
+        e.bLocalizedCountry = false;
+    }
 
     if (newKey) {
         //qDebug() << "inserting" << k << "=" << value;
@@ -158,6 +163,14 @@ bool KEntryMap::setEntry(const QByteArray &group, const QByteArray &key, const Q
         return true;
     } else {
 //                KEntry e2 = it.value();
+        if (options & EntryLocalized) {
+            // fast exit checks for cases where the existing entry is more specific
+            const KEntry &e2 = it.value();
+            if (e2.bLocalizedCountry && !e.bLocalizedCountry) {
+                // lang_COUNTRY > lang
+                return false;
+            }
+        }
         if (it.value() != e) {
             //qDebug() << "changing" << k << "from" << e.mValue << "to" << value;
             it.value() = e;

src/core/kconfigdata.h
@@ -36,7 +36,8 @@ struct KEntry {
     /** Constructor. @internal */
     KEntry()
         : mValue(), bDirty(false),
-          bGlobal(false), bImmutable(false), bDeleted(false), bExpand(false), bReverted(false) {}
+          bGlobal(false), bImmutable(false), bDeleted(false), bExpand(false), bReverted(false),
+          bLocalizedCountry(false) {}
     /** @internal */
     QByteArray mValue;
     /**
@@ -63,6 +64,11 @@ struct KEntry {
      * Entry has been reverted to its default value (from a more global file).
      */
     bool    bReverted: 1;
+    /**
+     * Entry is for a localized key. If @c false the value references just language e.g. "de",
+     * if @c true the value references language and country, e.g. "de_DE".
+     **/
+    bool    bLocalizedCountry: 1;
 };
 
 // These operators are used to check whether an entry which is about
@@ -165,6 +171,7 @@ public:
         EntryDeleted = 8,
         EntryExpansion = 16,
         EntryRawKey = 32,
+        EntryLocalizedCountry = 64,
         EntryDefault = (SearchDefaults << 16),
         EntryLocalized = (SearchLocalized << 16)
     };

src/core/kconfigini.cpp
@@ -85,6 +85,8 @@ KConfigIniBackend::parseConfig(const QByteArray &currentLocale, KEntryMap &entry
         return ParseOk;
     }
 
+    const QByteArray currentLanguage = currentLocale.split('_').first();
+
     bool bDefault = options & ParseDefaults;
     bool allowExecutableValues = options & ParseExpansions;
 
@@ -251,7 +253,7 @@ KConfigIniBackend::parseConfig(const QByteArray &currentLocale, KEntryMap &entry
             }
             printableToString(&aKey, file, lineNo);
             if (!locale.isEmpty()) {
-                if (locale != currentLocale) {
+                if (locale != currentLocale && locale != currentLanguage) {
                     // backward compatibility. C == en_US
                     if (locale.at(0) != 'C' || currentLocale != "en_US") {
                         if (merging) {
@@ -275,6 +277,9 @@ KConfigIniBackend::parseConfig(const QByteArray &currentLocale, KEntryMap &entry
             }
             if (!locale.isNull()) {
                 entryOptions |= KEntryMap::EntryLocalized;
+                if (locale.indexOf('_') != -1) {
+                    entryOptions |= KEntryMap::EntryLocalizedCountry;
+                }
             }
             printableToString(&line, file, lineNo);
             if (entryOptions & KEntryMap::EntryRawKey) {