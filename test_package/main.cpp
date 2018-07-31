2bb1fa33cb0c2ac504e60949d4300c67918f03c4
Ovidiu-Florin Bogdan <ovidiu-florin.bogdan@intel.com>
2018-07-31 22:20:02 +0300

    Actually write and read a config value in package test

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -4,13 +4,27 @@
 #include <QString>
 
 #include <KConfig>
+#include <KConfigGroup>
+
+static const QString TESTGROUP="General";
+static const QString TESTENTRY="TestEntry";
+static const QString TESTVALUE="TestValue";
 
 int main() {
 
     QString configPath = QString(BUILD_DIRECTORY) + "/test_config";
 
-    KConfig config(configPath, KConfig::NoGlobals);
+    KConfig config(configPath, KConfig::SimpleConfig);
+    KConfigGroup generalGroup( &config, TESTGROUP);
 
-    std::cout << "Test OK" << std::endl;
-    return 0;
+    generalGroup.writeEntry(TESTENTRY, TESTVALUE);
+    generalGroup.config()->sync();
+
+    QString readValue = generalGroup.readEntry(TESTENTRY, QString());
+
+    if (readValue == TESTVALUE) {
+        std::cout << "Test OK" << std::endl;
+        return 0;
+    }
+    return 1;
 }
