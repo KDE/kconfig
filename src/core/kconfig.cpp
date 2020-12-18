d9eb723d528c63265a6a534a3a6b660eded95d45
Albert Astals Cid <aacid@kde.org>
2020-12-18 19:16:53 +0100

    Remove ifdefs, we require Qt 5.14 now

--- src/core/kconfig.cpp
+++ src/core/kconfig.cpp
@@ -67,17 +67,10 @@ KConfigPrivate::KConfigPrivate(KConfig::OpenFlags flags,
     }
 
     static QBasicAtomicInt use_etc_kderc = Q_BASIC_ATOMIC_INITIALIZER(-1);
-#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
-    if (use_etc_kderc.load() < 0) {
-        use_etc_kderc.store( !qEnvironmentVariableIsSet("KDE_SKIP_KDERC"));    // for unit tests
-    }
-    if (use_etc_kderc.load()) {
-#else
     if (use_etc_kderc.loadRelaxed() < 0) {
         use_etc_kderc.storeRelaxed( !qEnvironmentVariableIsSet("KDE_SKIP_KDERC"));    // for unit tests
     }
     if (use_etc_kderc.loadRelaxed()) {
-#endif
         etc_kderc =
 #ifdef Q_OS_WIN
             QFile::decodeName(qgetenv("WINDIR") + "/kde5rc");
@@ -85,11 +78,7 @@ KConfigPrivate::KConfigPrivate(KConfig::OpenFlags flags,
             QStringLiteral("/etc/kde5rc");
 #endif
         if (!QFileInfo(etc_kderc).isReadable()) {
-#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
-            use_etc_kderc.store(false);
-#else
             use_etc_kderc.storeRelaxed(false);
-#endif
             etc_kderc.clear();
         }
     }
@@ -267,11 +256,7 @@ KConfig::KConfig(KConfigPrivate &d)
 KConfig::~KConfig()
 {
     Q_D(KConfig);
-#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
-    if (d->bDirty && (d->mBackend && d->mBackend->ref.load() == 1)) {
-#else
     if (d->bDirty && (d->mBackend && d->mBackend->ref.loadRelaxed() == 1)) {
-#endif
         sync();
     }
     delete d;
