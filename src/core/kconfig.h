c693c9eaae93eaed7589f0b4cd125ef297d82796
Friedrich W. H. Kossebau <kossebau@kde.org>
2023-03-21 15:43:33 +0100

    Remove unused private KConfig::keyList()
    NO_CHANGELOG
    
    (cherry picked from commit 114502c4c17425207a83e0fbe134eff20ac603f5)

--- src/core/kconfig.h
+++ src/core/kconfig.h
@@ -395,8 +395,6 @@ protected:
 private:
     friend class KConfigTest;
 
-    QStringList keyList(const QString &aGroup = QString()) const;
-
     Q_DISABLE_COPY(KConfig)
 
     Q_DECLARE_PRIVATE(KConfig)
