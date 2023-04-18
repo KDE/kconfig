KDE/kconfig

commit 76e5b4412782b9c1757c8cdb3984f99566a57b96

Fix kconf_update for spaces in paths
(CI says it happens on OSX)
Unittest it on Linux by exporting XDG_DATA_HOME with a space in it.

CCMAIL: kde-mac@kde.org

src/kconf_update/kconf_update.cpp
@@ -777,6 +777,7 @@ void KonfUpdate::gotScript(const QString &_script)
         log() << m_currentFilename << ": Running script '" << script << "'" << endl;
     }
 
+    QStringList args;
     QString cmd;
     if (interpreter.isEmpty()) {
         cmd = path;
@@ -787,12 +788,12 @@ void KonfUpdate::gotScript(const QString &_script)
             m_skip = true;
             return;
         }
-        cmd = interpreterPath + ' ' + path;
+        cmd = interpreterPath;
+        args << path;
     }
 
     if (!m_arguments.isNull()) {
-        cmd += ' ';
-        cmd += m_arguments;
+        args += m_arguments;
     }
 
     QTemporaryFile scriptIn;
@@ -834,7 +835,7 @@ void KonfUpdate::gotScript(const QString &_script)
             log() << "Script contents is:" << endl << scriptFile.readAll() << endl;
         }
     }
-    proc.start(cmd);
+    proc.start(cmd, args);
     if (!proc.waitForFinished(60000)) {
         logFileError() << "update script did not terminate within 60 seconds: " << cmd << endl;
         m_skip = true;

autotests/test_kconf_update.cpp
@@ -33,6 +33,12 @@
 
 QTEST_GUILESS_MAIN(TestKConfUpdate)
 
+void TestKConfUpdate::initTestCase()
+{
+    // Ensure it all works with spaces in paths (as happens more commonly on OSX where it's ~/Library/Application Support/)
+    qputenv("XDG_DATA_HOME", "/tmp/a b");
+}
+
 static void writeFile(const QString &path, const QString &content)
 {
     QFile file(path);

autotests/test_kconf_update.h
@@ -27,6 +27,7 @@ class TestKConfUpdate : public QObject
 {
     Q_OBJECT
 private Q_SLOTS:
+    void initTestCase();
     void test_data();
     void test();
     void testScript_data();

commit a0c398f020e84854d563fa4de68a5284742e5ce9

Fix kconf_update ScriptArguments containing more than one entry
https://invent.kde.org/frameworks/kconfig/-/commit/76e5b4412782b9c1757c8cdb3984f99566a57b96 broke this and based on the name
of the parameter, I'd expect that to be able to have multiple args

src/kconf_update/kconf_update.cpp
@@ -50,7 +50,6 @@ public:
 
     void gotId(const QString &_id);
     void gotScript(const QString &_script);
-    void gotScriptArguments(const QString &_arguments);
     void resetOptions();
 
 protected:
@@ -63,7 +62,7 @@ protected:
     QString m_id;
 
     bool m_bUseConfigInfo;
-    QString m_arguments;
+    QStringList m_arguments;
     QTextStream *m_textStream;
     QFile *m_file;
     QString m_line;
@@ -261,7 +260,8 @@ bool KonfUpdate::updateFile(const QString &filename)
             gotScript(m_line.mid(7));
             resetOptions();
         } else if (m_line.startsWith(QLatin1String("ScriptArguments="))) {
-            gotScriptArguments(m_line.mid(16));
+            const QString argLine = m_line.mid(16);
+            m_arguments = QProcess::splitCommand(argLine);
         } else {
             qCDebugFile(KCONF_UPDATE_LOG) << "Parse error";
         }
@@ -318,11 +318,6 @@ void KonfUpdate::gotId(const QString &_id)
     }
 }
 
-void KonfUpdate::gotScriptArguments(const QString &_arguments)
-{
-    m_arguments = _arguments;
-}
-
 void KonfUpdate::gotScript(const QString &_script)
 {
     QString script;
@@ -357,7 +352,7 @@ void KonfUpdate::gotScript(const QString &_script)
         }
     }
 
-    if (!m_arguments.isNull()) {
+    if (!m_arguments.isEmpty()) {
         qCDebug(KCONF_UPDATE_LOG) << m_currentFilename << ": Running script" << script << "with arguments" << m_arguments;
     } else {
         qCDebug(KCONF_UPDATE_LOG) << m_currentFilename << ": Running script" << script;
@@ -378,9 +373,7 @@ void KonfUpdate::gotScript(const QString &_script)
         args << path;
     }
 
-    if (!m_arguments.isNull()) {
-        args += m_arguments;
-    }
+    args += m_arguments;
 
     int result;
     qCDebug(KCONF_UPDATE_LOG) << "About to run" << cmd;

autotests/test_kconf_update.cpp
@@ -93,10 +93,15 @@ void TestKConfUpdate::testScript_data()
 
     QTest::newRow("should run command and modify file") << updContent << scriptContent << configIn << configOut;
 
-    const QString argScriptContent = QStringLiteral("sed -i \"s/secondVal=$1/secondVal=abc/\" %1; echo %1 $1").arg(confPath);
+    const QString argScriptContent = QStringLiteral("sed -i \"s/secondVal=$1/secondVal=abc/\" %1").arg(confPath);
     const QString argConfigOut = QStringLiteral("[grp]\nfirstVal=abc\nsecondVal=abc\n");
     QTest::newRow("should run command with argument and modify file")
         << QStringLiteral("ScriptArguments=xyz\n") + updContent << argScriptContent << configIn << argConfigOut;
+
+    const QString argScriptContent2 = QStringLiteral("sed -i \"s/secondVal=$1/secondVal=$2/\" %1").arg(confPath);
+    const QString argConfigOut2 = QStringLiteral("[grp]\nfirstVal=abc\nsecondVal=abc\n");
+    QTest::newRow("should run command with arguments and modify file")
+        << QStringLiteral("ScriptArguments=xyz abc\n") + updContent << argScriptContent << configIn << argConfigOut;
 }
 
 void TestKConfUpdate::testScript()