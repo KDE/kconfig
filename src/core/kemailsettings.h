d44b83d90d46a067d25bb8ee7b09ed5b308222d3
Thiago Sueto <herzenschein@gmail.com>
2023-06-28 22:07:38 +0000

    KEMailSettings

--- src/core/kemailsettings.h
+++ src/core/kemailsettings.h
@@ -17,11 +17,21 @@ class KEMailSettingsPrivate;
 /**
  * \class KEMailSettings kemailsettings.h <KEMailSettings>
  *
- * This is just a small class to facilitate accessing e-mail settings in
- * a sane way, and allowing any program to manage multiple e-mail
- * profiles effortlessly
+ * This is a small class that uses KConfig to facilitate accessing e-mail
+ * settings in a centralized manner, allowing any program to manage
+ * multiple e-mail profiles effortlessly.
  *
- * The default profile is automatically selected in the constructor.
+ * The email profile management is handled via the
+ * $XDG_CONFIG_DIR/emaildefaults configuration file. The main "[Defaults]"
+ * group should contain a "Profile" entry stating which profile is currently
+ * active, while profiles themselves are prefixed with "PROFILE_".
+ *
+ * Common email metadata is available via the Setting enum, which is used to
+ * read from emaildefaults using getSetting() and write to the disk with
+ * setSetting(), which runs KConfig::sync() internally.
+ *
+ * The default profile (PROFILE_Default) is automatically selected in the
+ * constructor.
  *
  * @author Alex Zepeda zipzippy@sonic.net
  **/
@@ -30,23 +40,36 @@ class KCONFIGCORE_EXPORT KEMailSettings
     Q_DECLARE_TR_FUNCTIONS(KEMailSettings)
 public:
     /**
-     * The list of settings that I thought of when I wrote this
-     * class.
+     * @brief A list of common metadata used for email.
+     *
      * @see getSetting()
      * @see setSetting()
      **/
     enum Setting {
+        /** The program used to manage email. */
         ClientProgram,
+        /** The preferred terminal of the user associated with the email. */
         ClientTerminal,
+        /** The name of the user associated with the email. */
         RealName,
+        /** The address of the user associated with the email. */
         EmailAddress,
+        /**
+         * The address used to reply to the user associated with the email,
+         * which may or may not be the same as EmailAddress.
+         */
         ReplyToAddress,
+        /** The organization the user belongs to. */
         Organization,
     };
 
     /**
-     * Default constructor, just sets things up and sets the default profile
-     * as the current profile
+     * @brief This constructs an object that points to the emaildefaults
+     * configuration file.
+     *
+     * Profile information is always read from and written to the
+     * emaildefaults file, and it is not possible to specify a different
+     * configuration file.
      **/
     KEMailSettings();
 
@@ -54,47 +77,59 @@ public:
     KEMailSettings &operator=(const KEMailSettings &) = delete;
 
     /**
-     * Default destructor, nothing to see here.
+     * @brief Default destructor.
      **/
     ~KEMailSettings();
 
     /**
-     * List of profiles available.
-     * @return the list of profiles
+     * @brief The list of profiles available.
+     *
+     * The list of profiles can be used to retrieve profile names, which can
+     * then be used with setProfile() and setDefault().
+     *
+     * @return The list of profiles in the emaildefaults configuration file.
      **/
     QStringList profiles() const;
 
     /**
-     * Change the current profile.
-     * @param s the name of the new profile
+     * @brief Change the current profile or create a new one.
+     *
+     * New profiles follow the pattern "PROFILE_" + newprofilename.
+     *
+     * @param s The name of the new profile.
      **/
     void setProfile(const QString &s);
 
     /**
-     * Returns the name of the default profile.
-     * @returns the name of the one that's currently default QString() if none
+     * @brief The name of the current default profile.
+     * @returns The name of the current default profile, or QString() if unset.
      **/
     QString defaultProfileName() const;
 
     /**
-     * Sets a new default.
-     * @param def the new default
+     * @brief Sets a new default profile.
+     * @param def The new default profile to be set.
      **/
     void setDefault(const QString &def);
 
     /**
-     * Get one of the predefined "basic" settings.
-     * @param s the setting to get
-     * @return the value of the setting, or QString() if not
-     *         set
+     * @brief The value assigned to one of the predefined entries from the
+     * @ref Setting enum.
+     *
+     * @param s The @ref Setting whose value will be returned.
+     *
+     * @return The value of the @ref Setting, or QString() if unset.
      **/
     QString getSetting(KEMailSettings::Setting s) const;
 
     /**
-     * Set one of the predefined "basic" settings.
-     * @param s the setting to set
-     * @param v the new value of the setting, or QString() to
-     *         unset
+     * @brief The value to be assigned to one of the predefined entries
+     * from the @ref Setting enum.
+     *
+     * The write operation will run KConfig::sync() after each write.
+     *
+     * @param s The @ref Setting that will be changed.
+     * @param v The new value of the @ref Setting, or QString() if unset.
      **/
     void setSetting(KEMailSettings::Setting s, const QString &v);
 
