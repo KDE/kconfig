b05af56a418e8add360b954b3ce92e039803fbe1
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-04-09 12:45:13 +0200

    autotests: Don't use a timeout in testLocalDeletion
    Timeouts cause unpredictable behaviour where a test may sometimes fail
    and sometimes pass.

--- autotests/kconfigtest.cpp
+++ autotests/kconfigtest.cpp
@@ -1754,8 +1754,8 @@ void KConfigTest::testLocalDeletion()
     {
         KConfig mainConfig(s_kconfig_test_subdir);
         KConfigGroup mainGroup(&mainConfig, QStringLiteral("OwnTestGroup"));
-        QTRY_COMPARE_WITH_TIMEOUT(mainGroup.readEntry("GlobalKey", "Default"), QStringLiteral("Default"), 5000); // key is gone
-        QTRY_COMPARE_WITH_TIMEOUT(mainGroup.keyList(), expectedKeys, 5000);
+        QCOMPARE(mainGroup.readEntry("GlobalKey", "Default"), QStringLiteral("Default")); // key is gone
+        QCOMPARE(mainGroup.keyList(), expectedKeys);
     }
 }
 
