74dd833fcbf12792512fabf6287e94f16cd69b64
Marius P <nmariusp1@gmail.com>
2024-07-01 21:29:07 +0300

    Fix two cmakelint types of issue
    Extra spaces between 'if' and its () [whitespace/extra]
    Weird indentation; use 2 spaces [whitespace/indent]

--- KF6ConfigMacros.cmake
+++ KF6ConfigMacros.cmake
@@ -53,46 +53,46 @@
 include(CMakeParseArguments)
 
 function(KCONFIG_ADD_KCFG_FILES _target_or_source_var)
-   set(options GENERATE_MOC USE_RELATIVE_PATH)
-   cmake_parse_arguments(ARG "${options}" "" "" ${ARGN})
+    set(options GENERATE_MOC USE_RELATIVE_PATH)
+    cmake_parse_arguments(ARG "${options}" "" "" ${ARGN})
 
-   if(TARGET ${_target_or_source_var})
-       get_target_property(aliased_target ${_target_or_source_var} ALIASED_TARGET)
-       if(aliased_target)
-           message(FATAL_ERROR "Target argument passed to kconfig_add_kcfg_files must not be an alias: ${_target_or_source_var}")
-       endif()
-   endif()
+    if(TARGET ${_target_or_source_var})
+        get_target_property(aliased_target ${_target_or_source_var} ALIASED_TARGET)
+        if(aliased_target)
+            message(FATAL_ERROR "Target argument passed to kconfig_add_kcfg_files must not be an alias: ${_target_or_source_var}")
+        endif()
+    endif()
 
-   set(sources)
-   foreach(_current_FILE ${ARG_UNPARSED_ARGUMENTS})
-       get_filename_component(_tmp_FILE ${_current_FILE} ABSOLUTE)
-       get_filename_component(_abs_PATH ${_tmp_FILE} PATH)
+    set(sources)
+    foreach(_current_FILE ${ARG_UNPARSED_ARGUMENTS})
+        get_filename_component(_tmp_FILE ${_current_FILE} ABSOLUTE)
+        get_filename_component(_abs_PATH ${_tmp_FILE} PATH)
 
-       if(ARG_USE_RELATIVE_PATH) # Process relative path only if the option was set
-           # Get relative path
-           get_filename_component(_rel_PATH ${_current_FILE} PATH)
+        if(ARG_USE_RELATIVE_PATH) # Process relative path only if the option was set
+            # Get relative path
+            get_filename_component(_rel_PATH ${_current_FILE} PATH)
 
-           if(IS_ABSOLUTE ${_rel_PATH})
-               # We got an absolute path
-               set(_rel_PATH "")
-           endif()
-       endif()
+            if(IS_ABSOLUTE ${_rel_PATH})
+                # We got an absolute path
+                set(_rel_PATH "")
+            endif()
+        endif()
 
-       get_filename_component(_basename ${_tmp_FILE} NAME_WE)
-       # If we had a relative path and we're asked to use it, then change the basename accordingly
-       if(_rel_PATH)
-           set(_basename ${_rel_PATH}/${_basename})
-       endif()
+        get_filename_component(_basename ${_tmp_FILE} NAME_WE)
+        # If we had a relative path and we're asked to use it, then change the basename accordingly
+        if(_rel_PATH)
+            set(_basename ${_rel_PATH}/${_basename})
+        endif()
 
-       # if the file name in the kcfgc is changed, we need to rerun cmake
-       set_property(DIRECTORY APPEND
-           PROPERTY CMAKE_CONFIGURE_DEPENDS ${_tmp_FILE}
-       )
+        # if the file name in the kcfgc is changed, we need to rerun cmake
+        set_property(DIRECTORY APPEND
+            PROPERTY CMAKE_CONFIGURE_DEPENDS ${_tmp_FILE}
+        )
 
-       file(READ ${_tmp_FILE} _contents)
-       string(REGEX MATCH "File=([^\n]+\\.kcfg)\n" "" "${_contents}")
-       set(_kcfg_FILENAME "${CMAKE_MATCH_1}")
-       if(NOT _kcfg_FILENAME)
+        file(READ ${_tmp_FILE} _contents)
+        string(REGEX MATCH "File=([^\n]+\\.kcfg)\n" "" "${_contents}")
+        set(_kcfg_FILENAME "${CMAKE_MATCH_1}")
+        if(NOT _kcfg_FILENAME)
             string(REGEX MATCH "File=([^\n]+\\.kcfg).*\n" "" "${_contents}")
             if(CMAKE_MATCH_1)
                 message(WARNING "${_tmp_FILE}: Broken \"File\" field, make sure it's pointing at the right file")
@@ -101,58 +101,58 @@ function(KCONFIG_ADD_KCFG_FILES _target_or_source_var)
                 message(WARNING "Couldn't read the \"File\" field in ${_tmp_FILE}")
                 set(_kcfg_FILENAME "${_basename}.kcfg")
             endif()
-       endif()
+        endif()
 
-       string(REGEX MATCH "HeaderExtension=([^\n]+)\n" "" "${_contents}")
-       if(CMAKE_MATCH_1)
+        string(REGEX MATCH "HeaderExtension=([^\n]+)\n" "" "${_contents}")
+        if(CMAKE_MATCH_1)
             set(_kcfg_header_EXT "${CMAKE_MATCH_1}")
-       else()
+        else()
             set(_kcfg_header_EXT "h")
-       endif()
+        endif()
 
-       set(_src_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
-       set(_header_FILE ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.${_kcfg_header_EXT})
-       set(_moc_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
-       set(_kcfg_FILE   ${_abs_PATH}/${_kcfg_FILENAME})
-       # Maybe the .kcfg is a generated file?
-       if(NOT EXISTS "${_kcfg_FILE}")
-           set(_kcfg_FILE   ${CMAKE_CURRENT_BINARY_DIR}/${_kcfg_FILENAME})
-       endif()
+        set(_src_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
+        set(_header_FILE ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.${_kcfg_header_EXT})
+        set(_moc_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
+        set(_kcfg_FILE   ${_abs_PATH}/${_kcfg_FILENAME})
+        # Maybe the .kcfg is a generated file?
+        if(NOT EXISTS "${_kcfg_FILE}")
+            set(_kcfg_FILE   ${CMAKE_CURRENT_BINARY_DIR}/${_kcfg_FILENAME})
+        endif()
 
-       if(NOT EXISTS "${_kcfg_FILE}")
-           message(FATAL_ERROR "${_kcfg_FILENAME} not found; tried in ${_abs_PATH} and ${CMAKE_CURRENT_BINARY_DIR}")
-       endif()
+        if(NOT EXISTS "${_kcfg_FILE}")
+            message(FATAL_ERROR "${_kcfg_FILENAME} not found; tried in ${_abs_PATH} and ${CMAKE_CURRENT_BINARY_DIR}")
+        endif()
 
-       # make sure the directory exist in the build directory
-       if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}")
-           file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH})
-       endif()
+        # make sure the directory exist in the build directory
+        if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}")
+            file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH})
+        endif()
 
-       # the command for creating the source file from the kcfg file
-       add_custom_command(OUTPUT ${_header_FILE} ${_src_FILE}
+        # the command for creating the source file from the kcfg file
+        add_custom_command(OUTPUT ${_header_FILE} ${_src_FILE}
                           COMMAND KF6::kconfig_compiler
                           ARGS ${_kcfg_FILE} ${_tmp_FILE} -d ${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}
                           MAIN_DEPENDENCY ${_tmp_FILE}
                           DEPENDS ${_kcfg_FILE} KF6::kconfig_compiler)
 
-       set_source_files_properties(${_header_FILE} ${_src_FILE} PROPERTIES
-           SKIP_AUTOUIC ON
-       )
+        set_source_files_properties(${_header_FILE} ${_src_FILE} PROPERTIES
+            SKIP_AUTOUIC ON
+        )
 
-       if(NOT ARG_GENERATE_MOC)
-           set_source_files_properties(${_header_FILE} ${_src_FILE} PROPERTIES
-               SKIP_AUTOMOC ON
+        if(NOT ARG_GENERATE_MOC)
+            set_source_files_properties(${_header_FILE} ${_src_FILE} PROPERTIES
+                SKIP_AUTOMOC ON
             )
-       endif()
+        endif()
 
-       list(APPEND sources ${_src_FILE} ${_header_FILE})
-   endforeach(_current_FILE)
+        list(APPEND sources ${_src_FILE} ${_header_FILE})
+    endforeach(_current_FILE)
 
-   if(TARGET ${_target_or_source_var})
-      target_sources(${_target_or_source_var} PRIVATE ${sources})
-   else()
-      set(${_target_or_source_var} ${${_target_or_source_var}} ${sources} PARENT_SCOPE)
-   endif()
+    if(TARGET ${_target_or_source_var})
+        target_sources(${_target_or_source_var} PRIVATE ${sources})
+    else()
+        set(${_target_or_source_var} ${${_target_or_source_var}} ${sources} PARENT_SCOPE)
+    endif()
 
 endfunction(KCONFIG_ADD_KCFG_FILES)
 
@@ -205,21 +205,21 @@ function(kconfig_target_kcfg_file ARG_TARGET)
     )
     cmake_parse_arguments(PARSE_ARGV 1 ARG "${_options}" "${_single_arguments}" "${_multi_arguments}")
 
-    if (NOT TARGET ${ARG_TARGET})
+    if(NOT TARGET ${ARG_TARGET})
         message(FATAL_ERROR "${ARG_TARGET} not found")
     endif()
 
-    if (NOT EXISTS "${ARG_FILE}" AND NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${ARG_FILE}")
+    if(NOT EXISTS "${ARG_FILE}" AND NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${ARG_FILE}")
         message(FATAL_ERROR "${ARG_FILE} not found")
     endif()
 
-    if ("${ARG_CLASS_NAME}" STREQUAL "")
+    if("${ARG_CLASS_NAME}" STREQUAL "")
         message(FATAL_ERROR "CLASS_NAME is a required argument")
     endif()
 
     set(_filepath "${ARG_FILE}")
     get_filename_component(_filename ${ARG_FILE} NAME)
-    if (IS_ABSOLUTE ${ARG_FILE})
+    if(IS_ABSOLUTE ${ARG_FILE})
         set(ARG_FILE ${_filename})
     endif()
 
@@ -231,21 +231,21 @@ function(kconfig_target_kcfg_file ARG_TARGET)
         endif()
 
         _convert_to_camelcase("${_option}" _entry)
-        if (ARG_${_option})
+        if(ARG_${_option})
             string(APPEND _content "${_entry}=true\n")
         endif()
     endforeach()
 
     foreach(_argument ${_single_arguments})
         _convert_to_camelcase("${_argument}" _entry)
-        if (ARG_${_argument})
+        if(ARG_${_argument})
             string(APPEND _content "${_entry}=${ARG_${_argument}}\n")
         endif()
     endforeach()
 
     foreach(_argument ${_multi_arguments})
         _convert_to_camelcase("${_argument}" _entry)
-        if (ARG_${_argument})
+        if(ARG_${_argument})
             list(JOIN ARG_${_argument} ", " _joined)
             string(APPEND _content "${_entry}=${_joined}\n")
         endif()
@@ -257,7 +257,7 @@ function(kconfig_target_kcfg_file ARG_TARGET)
 
     configure_file(${_filepath} "${CMAKE_CURRENT_BINARY_DIR}/${_filename}" COPYONLY)
 
-    if (${ARG_GENERATE_MOC})
+    if(${ARG_GENERATE_MOC})
         KCONFIG_ADD_KCFG_FILES(${ARG_TARGET} ${_output} GENERATE_MOC)
     else()
         KCONFIG_ADD_KCFG_FILES(${ARG_TARGET} ${_output})
