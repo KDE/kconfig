dac299e68ad18ca534e428a76a1733a8b1bcd1c5
Nicolas Fella <nicolas.fella@gmx.de>
2025-05-29 15:42:25 +0200

    Add Python bindings
    Co-authored-by: Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>

---
+++ python/bindings.h
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2024 Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#pragma once
+
+#include <KAuthorized>
+#include <KConfig>
+#include <KConfigBase>
+#include <KConfigGroup>
+#include <KConfigWatcher>
+#include <KCoreConfigSkeleton>
+#include <KDesktopFileAction>
+#include <KEMailSettings>
+#include <KSharedConfig>
