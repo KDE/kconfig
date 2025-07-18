4f93b5c5bf913e9e65d667294b5b8f68d7ae7ba2
Marco Martin <notmart@gmail.com>
2025-07-18 16:51:08 +0200

    KWindowStateSaverQuick: possibility to save more properties to config
    by specifying the extraProperties variable in WindowStateSaver is
    possible to read and save more properties to disk as part of the
    application state
    
    This is intended for more things to save an restore which ar not
    really settings but things like if some sidebar is present,
    the size of the sidebar, size of icons and things like that
    
        Config.WindowStateSaver {
            configGroupName: "Window-main"
            extraProperties: ["sidebarState"]
        }
    
        property alias sidebarState: root.pageStack.savedState

--- src/qml/kwindowstatesaverquick.cpp
+++ src/qml/kwindowstatesaverquick.cpp
@@ -4,10 +4,55 @@
 #include "kwindowstatesaverquick.h"
 #include "kconfig_qml_log_settings.h"
 
+#include <QQmlProperty>
 #include <QQuickItem>
 #include <QQuickWindow>
 
+#include <KSharedConfig>
 #include <KWindowStateSaver>
+
+PropertyObject::PropertyObject(QObject *sender, const QString &configGroupName, const QString &property, QObject *parent)
+    : QObject(parent)
+    , m_sender(sender)
+    , m_configGroupName(configGroupName)
+    , m_propertyName(property)
+{
+    const QQmlProperty prop(sender, property);
+    if (prop.isValid()) {
+        prop.connectNotifySignal(this, SLOT(propertyChanged()));
+
+        KConfigGroup cg(KSharedConfig::openStateConfig(), m_configGroupName);
+
+        if (cg.hasKey(m_propertyName)) {
+            QVariant var = prop.read();
+
+            switch (var.typeId()) {
+            case QMetaType::Int:
+                prop.write(cg.readEntry(m_propertyName, 0));
+                break;
+            case QMetaType::Double:
+                prop.write(cg.readEntry(m_propertyName, 0.0));
+                break;
+            case QMetaType::QString:
+                prop.write(cg.readEntry(m_propertyName, QString()));
+                break;
+            case QMetaType::QStringList:
+                prop.write(cg.readEntry(m_propertyName, QStringList()));
+                break;
+            case QMetaType::QByteArray:
+                prop.write(cg.readEntry(m_propertyName, QByteArray()));
+                break;
+            }
+        }
+    }
+}
+
+void PropertyObject::propertyChanged()
+{
+    const QVariant newProperty = m_sender->property(m_propertyName.toLatin1().data());
+    KConfigGroup cg(KSharedConfig::openStateConfig(), m_configGroupName);
+    cg.writeEntry(m_propertyName, newProperty);
+}
 
 void KWindowStateSaverQuick::classBegin()
 {
@@ -49,6 +94,33 @@ void KWindowStateSaverQuick::setConfigGroupName(const QString &name)
 QString KWindowStateSaverQuick::configGroupName() const
 {
     return m_configGroupName;
+}
+
+QStringList KWindowStateSaverQuick::extraProperties() const
+{
+    return m_extraProperties;
+}
+
+void KWindowStateSaverQuick::setExtraProperties(const QStringList &properties)
+{
+    if (m_extraProperties == properties) {
+        return;
+    }
+
+    m_propertyObjects.clear();
+
+    m_extraProperties = properties;
+
+    const auto parentItem = qobject_cast<QQuickItem *>(parent());
+    const auto window = parentItem ? qobject_cast<QWindow *>(parentItem->window()) : nullptr;
+
+    if (window && !m_extraProperties.isEmpty()) {
+        for (const QString &propName : std::as_const(m_extraProperties)) {
+            m_propertyObjects[propName] = std::make_unique<PropertyObject>(window, m_configGroupName, propName);
+        }
+    }
+
+    Q_EMIT extraPropertiesChanged();
 }
 
 #include "moc_kwindowstatesaverquick.cpp"
