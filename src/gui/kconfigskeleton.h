ee28be847bc985df0e0b05cb9b58c7372006ebc4
Thiago Sueto <herzenschein@gmail.com>
2026-02-27 18:50:02 -0300

    QDoc KConfigSkeleton

--- src/gui/kconfigskeleton.h
+++ src/gui/kconfigskeleton.h
@@ -22,7 +22,7 @@
  *
  * \brief Class for handling preferences settings for an application.
  *
- * This class extends KCoreConfigSkeleton by support for GUI types.
+ * This class extends KCoreConfigSkeleton with support for GUI types.
  */
 class KCONFIGGUI_EXPORT KConfigSkeleton : public KCoreConfigSkeleton
 {
@@ -30,7 +30,7 @@ class KCONFIGGUI_EXPORT KConfigSkeleton : public KCoreConfigSkeleton
 public:
     /*!
      * \inmodule KConfigGui
-     * Class for handling a color preferences item.
+     * \brief Class for handling a color preferences item.
      */
     class KCONFIGGUI_EXPORT ItemColor : public KConfigSkeletonGenericItem<QColor>
     {
@@ -49,7 +49,7 @@ public:
 
     /*!
      * \inmodule KConfigGui
-     * Class for handling a font preferences item.
+     * \brief Class for handling a font preferences item.
      */
     class KCONFIGGUI_EXPORT ItemFont : public KConfigSkeletonGenericItem<QFont>
     {
@@ -68,62 +68,53 @@ public:
 
 public:
     /*!
-     * Constructor.
+     * Constructs a configuration skeleton with the given \a configname
+     * as a child of \a parent.
      *
-     * \a configname name of config file. If no name is given, the default
-     * config file as returned by KSharedConfig::openConfig() is used.
+     * If no name is given, the default config file
+     * as returned by KSharedConfig::openConfig() is used.
      */
     explicit KConfigSkeleton(const QString &configname = QString(), QObject *parent = nullptr);
 
     /*!
-     * Constructor.
-     *
-     * \a config configuration object to use.
+     * Constructs a configuration skeleton with the given \a config object
+     * as a child of \a parent.
      */
     explicit KConfigSkeleton(KSharedConfig::Ptr config, QObject *parent = nullptr);
 
     /*!
-     * Constructor
+     * Constructs a configuration skeleton with a given \a config object
+     * as a child of \a parent.
      *
-     * \a config configuration object to use.
+     * The \a value must be KCoreConfigSkeleton::IsStdUniqPtr.
      *
-     * \a value must be KCoreConfigSkeleton::DisambiguateConstructor::IsStdUniqPtr
+     * The use of this single-valued enum is mandatory to disambiguate with the other constructor
+     * that may otherwise be ambiguous in case of default nullptr
+     * KCoreConfigSkeleton(KSharedConfig::Ptr(nullptr)) or
+     * KCoreConfigSkeleton(std::unique_ptr<KConfig> (nullptr)) with parent = nullptr.
      *
      * \since 6.23
+     * \sa KCoreConfigSkeleton::DisambiguateConstructor
      */
     explicit KConfigSkeleton(std::unique_ptr<KConfig> config, KCoreConfigSkeleton::DisambiguateConstructor value, QObject *parent = nullptr);
 
     /*!
-     * Register an item of type QColor.
+     * Registers a QColor item with the given \a name,
+     * the initial value \a reference to hold in the item that is set by read() calls
+     * and read by save() calls, and the \a defaultValue for the item used
+     * when the config file does not yet contain the \a key of this item.
      *
-     * \a name Name used to identify this setting. Names must be unique.
-     *
-     * \a reference Pointer to the variable, which is set by read()
-     * calls and read by save() calls.
-     *
-     * \a defaultValue Default value, which is used when the config file
-     * does not yet contain the key of this item.
-     *
-     * \a key Key used in config file. If \a key is a null string, \a name is used as key.
-     *
-     * Returns the created item
+     * If \a key is a null string, \a name is used as key.
      */
     ItemColor *addItemColor(const QString &name, QColor &reference, const QColor &defaultValue = QColor(128, 128, 128), const QString &key = QString());
 
     /*!
-     * Register an item of type QFont.
+     * Registers a QFont item with the given \a name,
+     * the initial value \a reference to hold in the item that is set by read() calls
+     * and read by save() calls, and the \a defaultValue for the item used
+     * when the config file does not yet contain the \a key of this item.
      *
-     * \a name Name used to identify this setting. Names must be unique.
-     *
-     * \a reference Pointer to the variable, which is set by read()
-     * calls and read by save() calls.
-     *
-     * \a defaultValue Default value, which is used when the config file
-     * does not yet contain the key of this item.
-     *
-     * \a key Key used in config file. If \a key is a null string, \a name is used as key.
-     *
-     * Returns the created item
+     * If \a key is a null string, \a name is used as key.
      */
     ItemFont *addItemFont(const QString &name, QFont &reference, const QFont &defaultValue = QFont(), const QString &key = QString());
 };
