41dfeb505fa1e02891a4a98ff576fc9ba81c546a
Thiago Sueto <herzenschein@gmail.com>
2026-03-03 17:32:41 -0300

    QDoc KDesktopFile

--- src/core/kdesktopfile.h
+++ src/core/kdesktopfile.h
@@ -15,40 +15,32 @@ class KDesktopFilePrivate;
 
 /*!
  * \class KDesktopFile
- * \inmodule KConfigGui
+ * \inmodule KConfigCore
  *
  * This class implements KDE's support for the freedesktop.org
- * \l {https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html} {Desktop Entry Spec}.
+ * \l {https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html}
+ * {Desktop Entry Spec}.
  *
  * \sa KConfigBase
  * \sa KConfig
+ * \sa KDesktopFileAction
  */
 class KCONFIGCORE_EXPORT KDesktopFile : public KConfig
 {
 public:
     /*!
-     * Constructs a KDesktopFile object.
+     * Constructs a KDesktopFile object in \a resourceType with the given \a fileName.
      *
-     * See QStandardPaths for more information on resources.
-     *
-     * \a resourceType Allows you to change what sort of resource
-     *                       to search for if \a fileName is not absolute.
-     *                       For instance, you might want to specify GenericConfigLocation.
-     *
-     * \a fileName The name or path of the desktop file. If it
-     *                       is not absolute, it will be located
-     *                       using the resource type \a resourceType.
+     * The \a resourceType allows to change where to search
+     * for the desktop file if \a fileName is not absolute.
+     * For instance, you might want to specify GenericConfigLocation.
+     * \sa QStandardPaths::StandardLocation
      */
     explicit KDesktopFile(QStandardPaths::StandardLocation resourceType, const QString &fileName);
 
     /*!
-     * Constructs a KDesktopFile object.
-     *
-     * See QStandardPaths for more information on resources.
-     *
-     * \a fileName The name or path of the desktop file. If it
-     *                       is not absolute, it will be located
-     *                       using the resource type ApplicationsLocation
+     * Constructs a KDesktopFile object with the given \a fileName in its standard location QStandardPaths::ApplicationsLocation.
+     * \sa QStandardPaths::ApplicationsLocation
      */
     explicit KDesktopFile(const QString &fileName);
 
@@ -60,38 +52,57 @@ public:
     ~KDesktopFile() override;
 
     /*!
-     * Checks whether this is really a desktop file.
+     * Returns whether the file in the given \a path is a desktop file.
      *
      * The check is performed looking at the file extension (the file is not
      * opened).
      * Currently, the only valid extension is ".desktop".
-     *
-     * \a path the path of the file to check
-     *
-     * Returns \c true if the file appears to be a desktop file.
      */
     static bool isDesktopFile(const QString &path);
 
     /*!
-     * Checks whether the user is authorized to run this desktop file.
+     * Returns whether the user is authorized to run desktop file at the given \a path.
+     *
      * By default users are authorized to run all desktop files but
-     * the KIOSK framework can be used to activate certain restrictions.
-     * See README.kiosk for more information.
+     * the Kiosk framework can be used to activate certain restrictions.
      *
      * Note that desktop files that are not in a standard location (as
-     * specified by XDG_DATA_DIRS) must have their executable bit set
-     * to be authorized, regardless of KIOSK settings, to prevent users
+     * specified by \c XDG_DATA_DIRS) must have their executable bit set
+     * to be authorized regardless of Kiosk settings to prevent users
      * from inadvertently running trojan desktop files.
-     *
-     * \a path the file to check
-     *
-     * Returns \c true if the user is authorized to run the file
+     * \sa KAuthorized
+     * \sa {https://develop.kde.org/docs/administration/kiosk/}{Kiosk - Simple configuration management for large deployment}
      */
     static bool isAuthorizedDesktopFile(const QString &path);
 
     /*!
-     * Returns the location where changes for the .desktop file \a path
-     * should be written to.
+     * Returns the appropriate location to use to write changes to the desktop file based on the given \a path.
+     *
+     * If the \a path is non-writable (such as /usr/share/applications),
+     * this function returns the appropriate equivalent.
+     *
+     * If the appropriate location cannot be determined,
+     * the writable location will be in QStandardPaths::GenericDataLocation.
+     *
+     * For example:
+     *
+     * \table
+     * \header
+     *   \li Path
+     *   \li Writable Location
+     * \row
+     *   \li /etc/xdg/autostart/myapp.desktop
+     *   \li ~/.config/autostart/myapp.desktop
+     * \row
+     *   \li /usr/share/applications/myapp.desktop
+     *   \li ~/.local/share/applications/myapp.desktop
+     * \row
+     *   \li ~/Documents/myapp.desktop
+     *   \li ~/.local/share/myapp.desktop
+     * \row
+     *   \li myapp.desktop
+     *   \li ~/.local/share/myapp.desktop
+     * \endtable
      */
     static QString locateLocal(const QString &path);
 
@@ -101,92 +112,86 @@ public:
     KConfigGroup desktopGroup() const;
 
     /*!
-     * Returns the value of the "Type=" entry, or QString() if not specified
+     * Returns the value of the "Type=" entry, or QString() if not specified.
      */
     QString readType() const;
 
     /*!
-     * Returns the value of the "Icon=" entry, or QString() if not specified
+     * Returns the value of the "Icon=" entry, or QString() if not specified.
      */
     QString readIcon() const;
 
     /*!
-     * Returns the value of the "Name=" entry, or QString() if not specified
+     * Returns the value of the "Name=" entry, or QString() if not specified.
      */
     QString readName() const;
 
     /*!
-     * Returns the value of the "Comment=" entry, or QString() if not specified
+     * Returns the value of the "Comment=" entry, or QString() if not specified.
      */
     QString readComment() const;
 
     /*!
-     * Returns the value of the "GenericName=" entry, or QString() if not specified
+     * Returns the value of the "GenericName=" entry, or QString() if not specified.
      */
     QString readGenericName() const;
 
     /*!
-     * Returns the value of the "Path=" entry, or QString() if not specified
+     * Returns the value of the "Path=" entry, or QString() if not specified.
      */
     QString readPath() const;
 
     /*!
-     * Returns the value of the "URL=" entry, or QString() if not specified
+     * Returns the value of the "URL=" entry, or QString() if not specified.
      */
     QString readUrl() const;
 
     /*!
-     * Returns a list of the "Actions=" entries
+     * Returns a list of the "Actions=" entries.
      */
     QStringList readActions() const;
 
     /*!
-     * Returns a list of the "MimeType=" entries
+     * Returns a list of the "MimeType=" entries.
      * \since 5.15
      */
     QStringList readMimeTypes() const;
 
     /*!
-     * Sets the desktop action group.
-     *
-     * \a group the new action group
+     * Sets a new desktop action \a group.
      */
     KConfigGroup actionGroup(const QString &group);
 
     /*!
-     * Sets the desktop action group.
-     *
-     * \a group the new action group
+     * Sets a new desktop action \a group.
      */
     KConfigGroup actionGroup(const QString &group) const;
 
     /*!
-     * Returns \c true if the action group exists, \c false otherwise
-     *
-     * \a group the action group to test
+     * Returns whether the given action \a group exists.
      */
     bool hasActionGroup(const QString &group) const;
 
     /*!
-     * Checks whether there is a "Type=Link" entry.
+     * Returns whether there is a "Type=Link" entry.
      *
      * The link points to the "URL=" entry.
      */
     bool hasLinkType() const;
 
     /*!
-     * Checks whether there is an entry "Type=Application".
+     * Returns whether there is an entry "Type=Application".
      */
     bool hasApplicationType() const;
 
     /*!
-     * Checks whether there is an entry "Type=FSDevice".
+     * Returns whether there is an entry "Type=FSDevice".
      */
     bool hasDeviceType() const;
 
     /*!
-     * Checks whether the TryExec field contains a binary
-     * which is found on the local system.
+     * Returns whether the "TryExec=" field contains a binary
+     * that is found on the local system.
      */
     bool tryExec() const;
 
@@ -196,9 +201,9 @@ public:
     QString readDocPath() const;
 
     /*!
-     * Whether the entry should be suppressed in menus.
+     * Returns whether the entry should be suppressed in menus.
      *
-     * This handles the NoDisplay key
+     * This handles the "NoDisplay=" key.
      * \since 4.1
      */
     bool noDisplay() const;
@@ -209,8 +214,6 @@ public:
      *
      * Actual saving to \a file happens when the returned object is
      * destructed or when sync() is called upon it.
-     *
-     * \a file the new KDesktopFile object it will save itself to.
      */
     KDesktopFile *copyTo(const QString &file) const;
 
@@ -220,8 +223,7 @@ public:
     QString fileName() const;
 
     /*!
-     * Returns the desktop file's actions
-     *
+     * Returns the desktop file's actions.
      * \since 6.0
      */
     QList<KDesktopFileAction> actions() const;
