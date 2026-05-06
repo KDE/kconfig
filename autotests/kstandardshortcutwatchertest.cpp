15b1742b808bb15a724a3da3becb84e0f3385095
Noah Davis <noahadvs@gmail.com>
2026-05-06 14:03:53 -0400

    KStandardShortcut: Fix using a null key sequence for removed shortcuts
    Currently, removing a shortcut in system settings sets its string value in kdeglobals to an empty string. It may have been that we set the value to "none" in the past, but not anymore. Since the code before this patch only checked for "none", the empty string was converted to a null character key and KStandardShortcuts would give a null key sequence for the removed shortcut. This would cause disabled keys to trigger the removed shortcut since disabled keys can still send null character key events. For backwards compatibility, it may be a good idea to keep the check for "none".
    
    Example of how removed shortcuts are currently set in kdeglobals:
    
    ```
    [Shortcuts]
    Help=
    ```
    
    BUG: 518683
    FIXED-IN: 6.27

--- autotests/kstandardshortcutwatchertest.cpp
+++ autotests/kstandardshortcutwatchertest.cpp
@@ -21,6 +21,7 @@ private Q_SLOTS:
     void init();
     void testSignal();
     void testDataUpdated();
+    void testRemovedShortcut();
 };
 
 Q_DECLARE_METATYPE(KStandardShortcut::StandardShortcut)
@@ -67,6 +68,53 @@ void KStandardShortcutWatcherTest::testDataUpdated()
     group.sync();
     QTRY_COMPARE(signalSpy.count(), 1);
     QCOMPARE(KStandardShortcut::open(), newShortcut);
+}
+
+void KStandardShortcutWatcherTest::testRemovedShortcut()
+{
+#ifdef Q_OS_WIN
+    QSKIP("KConfig is built without DBus on Windows");
+#endif
+    static const auto shandardShortcut = KStandardShortcut::Help;
+    using QKeySequenceList = QList<QKeySequence>;
+    QSignalSpy signalSpy(KStandardShortcut::shortcutWatcher(), //
+                         &KStandardShortcut::StandardShortcutWatcher::shortcutChanged);
+    auto lastChanged = signalSpy.crend();
+    static const QKeySequenceList emptyShortcut{};
+    auto changeAndVerify = [&]() {
+        // Later, we need to assume that the old shortcut is not empty.
+        const auto oldShortcut = KStandardShortcut::shortcut(shandardShortcut);
+        QVERIFY(oldShortcut != emptyShortcut);
+
+        KStandardShortcut::saveShortcut(shandardShortcut, emptyShortcut);
+        // QTRY_* macros wait 5 seconds before checking. We're doing the wait
+        // separately so that we don't need to get and verify in one line of code.
+        QVERIFY(signalSpy.wait(std::chrono::seconds{5}));
+        auto it = std::find_if(signalSpy.crbegin(), signalSpy.crend(), [](const QVariantList &list) {
+            return list[0].value<KStandardShortcut::StandardShortcut>() == shandardShortcut;
+        });
+        // Verify that a new change happened.
+        QVERIFY(it != signalSpy.crend());
+        QVERIFY(it != lastChanged);
+        lastChanged = it;
+        // Before StandardShortcutWatcher::shortcutChanged is emitted,
+        // StandardShortcutWatcher calls KStandardShortcut::initialize.
+        // Verify that the signal sent the correct shortcut.
+        QCOMPARE((*it)[1].value<QKeySequenceList>(), emptyShortcut);
+        // Verify that KStandardShortcut::shortcut returns the same thing.
+        QCOMPARE(KStandardShortcut::shortcut(shandardShortcut), emptyShortcut);
+    };
+
+    // Later, we need to assume that the default shortcut is being used.
+    QCOMPARE(KStandardShortcut::shortcut(shandardShortcut), KStandardShortcut::hardcodedDefaultShortcut(shandardShortcut));
+    // Verify change to empty with default being used first.
+    changeAndVerify();
+
+    // Verify change to empty with non-default being used first.
+    const QKeySequenceList nonDefaultShortcut{QKeyCombination{Qt::KeyboardModifierMask, Qt::Key_Idiaeresis}};
+    KStandardShortcut::saveShortcut(shandardShortcut, nonDefaultShortcut);
+    QVERIFY(signalSpy.wait(std::chrono::seconds{5}));
+    changeAndVerify();
 }
 
 QTEST_MAIN(KStandardShortcutWatcherTest)
